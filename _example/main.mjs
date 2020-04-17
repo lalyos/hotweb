9f798d1cc377e48e4baefc00ba6743ea7bd5cf48
lalyos <lalyos@yahoo.com>
2020-04-17 09:42:27 +0200

    make example work with or without hotweb

--- _example/main.mjs
+++ _example/main.mjs
@@ -1,12 +1,17 @@
 import "https://use.fontawesome.com/releases/v5.3.1/js/all.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/mithril/2.0.4/mithril.min.js";
 
-import * as hotweb from '/_hotweb.mjs';
-import * as page from '/lib/page.mjs';
+import * as page from "/lib/page.mjs";
+import("/_hotweb.mjs")
+  .then((hotweb) => {
+    hotweb.watchCSS();
+    hotweb.watchHTML();
+    hotweb.refresh(() => m.redraw());
+  })
+  .catch((error) => {
+    console.log("[warning] hotweb couldn't be loaded:", error);
+  });
 
-hotweb.watchCSS();
-hotweb.watchHTML();
-hotweb.refresh(() => m.redraw())
 m.mount(document.body, wrap(() => page.Page));
 
 function wrap(cb) {
